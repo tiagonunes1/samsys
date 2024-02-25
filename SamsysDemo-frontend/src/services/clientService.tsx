1d4dced8bbde62d3af2c2b1184bb3a9277e25531
tiagonunes1 <tiagoalexandrenunes1@gmail.com>
2024-02-25 12:08:49 +0000

    Add Create method to the ClientService class

--- SamsysDemo-frontend/src/services/clientService.tsx
+++ SamsysDemo-frontend/src/services/clientService.tsx
@@ -5,75 +5,121 @@ import { MessagingHelper } from "../models/helper/messagingHelper";
 
 var apiBaseUrl = process.env.REACT_APP_API_URL;
 export class ClientService {
-    async Get(id: number): Promise<MessagingHelper<ClientDTO | null>> {
-        try {
-            const result = await axios.get(`${apiBaseUrl}client/${id}`, {
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json"
-                },
-            });
+  async Get(id: number): Promise<MessagingHelper<ClientDTO | null>> {
+    try {
+      const result = await axios.get(`${apiBaseUrl}client/${id}`, {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
 
-            return result.data;
-        }
-        catch (ex) {
-            return new MessagingHelper<null>(false, "Ocorreu um erro inesperado ao obter o cliente", null)
-        }
+      return result.data;
+    } catch (ex) {
+      return new MessagingHelper<null>(
+        false,
+        "Ocorreu um erro inesperado ao obter o cliente",
+        null
+      );
     }
+  }
 
-    async Update(id: number, dto: ClientEditDTO): Promise<MessagingHelper<ClientDTO | null>> {
-        try {
-            const result = await axios.put(`${apiBaseUrl}client/${id}`,
-                {
-                    ...dto
-                },
-                {
-                    headers: {
-                        Accept: "application/json",
-                        "Content-Type": "application/json"
-                    },
-                });
+  async Update(
+    id: number,
+    dto: ClientEditDTO
+  ): Promise<MessagingHelper<ClientDTO | null>> {
+    try {
+      const result = await axios.put(
+        `${apiBaseUrl}client/${id}`,
+        {
+          ...dto,
+        },
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-            return result.data;
-        }
-        catch (ex) {
-            return new MessagingHelper<null>(false, "Ocorreu um erro inesperado ao atualizar o cliente", null)
-        }
+      return result.data;
+    } catch (ex) {
+      return new MessagingHelper<null>(
+        false,
+        "Ocorreu um erro inesperado ao atualizar o cliente",
+        null
+      );
     }
-
-    async Enable(id: number): Promise<MessagingHelper<null>> {
-        try {
-            const result = await axios.post(`${apiBaseUrl}client/${id}/Enable`,
-                {},
-                {
-                    headers: {
-                        Accept: "application/json",
-                        "Content-Type": "application/json"
-                    },
-                });
-
-            return result.data;
+  }
+  async Create(
+    clientData: ClientCreateDTO
+  ): Promise<MessagingHelper<ClientCreateDTO | null>> {
+    try {
+      const result = await axios.post(
+        `${apiBaseUrl}client/create`,
+        {
+          ...clientData,
+        },
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
         }
-        catch (ex) {
-            return new MessagingHelper<null>(false, "Ocorreu um erro inesperado ao ativar o cliente", null)
-        }
+      );
+
+      return result.data;
+    } catch (ex) {
+      return new MessagingHelper<null>(
+        false,
+        "Ocorreu um erro ao criar o cliente",
+        null
+      );
     }
-
-    async Disable(id: number): Promise<MessagingHelper<null>> {
-        try {
-            const result = await axios.post(`${apiBaseUrl}client/${id}/Disable`,
-                {},
-                {
-                    headers: {
-                        Accept: "application/json",
-                        "Content-Type": "application/json"
-                    },
-                });
-
-            return result.data;
+  }
+  async Enable(id: number): Promise<MessagingHelper<null>> {
+    try {
+      const result = await axios.post(
+        `${apiBaseUrl}client/${id}/Enable`,
+        {},
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
         }
-        catch (ex) {
-            return new MessagingHelper<null>(false, "Ocorreu um erro inesperado ao desativar o cliente", null)
-        }
+      );
+
+      return result.data;
+    } catch (ex) {
+      return new MessagingHelper<null>(
+        false,
+        "Ocorreu um erro inesperado ao ativar o cliente",
+        null
+      );
     }
+  }
+
+  async Disable(id: number): Promise<MessagingHelper<null>> {
+    try {
+      const result = await axios.post(
+        `${apiBaseUrl}client/${id}/Disable`,
+        {},
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      return result.data;
+    } catch (ex) {
+      return new MessagingHelper<null>(
+        false,
+        "Ocorreu um erro inesperado ao desativar o cliente",
+        null
+      );
+    }
+  }
 }
